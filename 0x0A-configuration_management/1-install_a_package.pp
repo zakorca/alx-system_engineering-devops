9462fb70558813b40a91de41148d26c859226d86
ZAKARIA TOURAS <zakariatouras47@gmail.com>
2024-06-28 18:38:23 -0700

    task 1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# install flask from pip3 using Puppet.
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
